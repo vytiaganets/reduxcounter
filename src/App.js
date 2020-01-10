e27465f032c0b486660bda6cdc37797d2cc11541
Andrii <vytiaganets@ukr.net>
2020-01-10 14:17:49 +0200

    Redux with React

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {connect} from "react-redux";
+import Counter from "./Counter";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//Зіставлення стану Redux з властивостями компоненту
+function mapStateToProps(state) {
+  return {
+    countValue: state.count,
+  };
 }
 
-export default App;
+// Дія
+var increaseAction = {type: "increase"};
+var decreaseAction = {type: "decrease"};
+
+//Зіставлення дій Redux з властивостями компоненту
+function mapDispatchToProps(dispatch) {
+  return {
+    increaseCount: function() {
+      return dispatch(increaseAction);
+    },
+    decreaseCount: function() {
+      return dispatch(decreaseAction);
+    }
+  };
+}
+
+// HOC-компонент
+var connectedComponent = connect(
+  mapStateToProps,
+  mapDispatchToProps
+) (Counter);
+
+export default connectedComponent;
